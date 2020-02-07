8760f4bc4574c679bc9d639f95ab36415f223b5c
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-02-07 17:15:29 +0000

    MINOR: PSR2 sweep

--- tests/BreadcrumbNavigationTest.php
+++ tests/BreadcrumbNavigationTest.php
@@ -4,21 +4,25 @@ namespace Exadium\BreadcrumbNavigation\Tests;
 
 use \SilverStripe\Dev\SapphireTest;
 
-class BreadcrumbNavigationTest extends SapphireTest {
-	public function testPages() {
-		$this->markTestSkipped('TODO');
-	}
+class BreadcrumbNavigationTest extends SapphireTest
+{
+    public function testPages()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testCreateBreadcrumb() {
-		$this->markTestSkipped('TODO');
-	}
+    public function testCreateBreadcrumb()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testAddBreadcrumbAfter() {
-		$this->markTestSkipped('TODO');
-	}
+    public function testAddBreadcrumbAfter()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testAddBreadcrumbBefore() {
-		$this->markTestSkipped('TODO');
-	}
-
+    public function testAddBreadcrumbBefore()
+    {
+        $this->markTestSkipped('TODO');
+    }
 }
